69ca731a86c7ca385d6b897ae1921acb815a4247
vedeshwarJ <jupallivedeshwar@gmail.com>
2025-07-16 15:50:34 +0530

    factorial using DP

---
+++ DynamicPrograming/factorialDP.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class factorialDP{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(fact(n,dp));
+        sc.close();
+    }
+    public static int fact(int n,int[] dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return n*fact(n-1,dp);
+    }
+}
