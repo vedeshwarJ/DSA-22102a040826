995c3a229bf00792eb935a7b1eb87751d3f6cca3
vedeshwarJ <jupallivedeshwar@gmail.com>
2025-07-17 09:15:58 +0530

    sumofnaturals using dp added

---
+++ DynamicPrograming/sumofnaturalsDP.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sumofnaturals{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(Sum(n,dp));
+        sc.close();
+    }
+    public static int Sum(int n,int[] dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return n+Sum(n-1,dp);
+    }
+}
